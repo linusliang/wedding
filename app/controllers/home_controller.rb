9cfbbfa20732b9587c3b2d604fa960abac3ad6fc
Linus Liang <linusliang@gmail.com>
2018-01-04 13:16:24 -0800

    wrong hashtag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
 	require 'mini_magick'
 	require 'tempfile'
 
-	$hashtag = 'disneyworld'
+	$hashtag = 'instaprinterfun'
 
 	Aws.config.update({
 		region: 'us-west-1',
