da8fce1a642643e77aa036be559f5bfef3c36989
Linus Liang <linusliang@gmail.com>
2018-01-04 15:02:26 -0800

    changed refresh to 15 seconds

--- refresh_script.rb
+++ refresh_script.rb
@@ -3,7 +3,7 @@
 while true
 	begin		
 		system('wget --quiet -O - http://production.4dyvwfpdk3.us-west-1.elasticbeanstalk.com/print_new_pics')
-		sleep 30
+		sleep 15
 	rescue Exception => e 
 		Rails.logger.debug "**************** ERROR IN refresh script ****************"
 		Rails.logger.debug e.message  		
