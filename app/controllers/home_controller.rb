8bf92587185b44c607c8ac99c5ecd7e3a9994a7e
Linus Liang <linusliang@gmail.com>
2018-03-12 01:14:26 -0700

    50 px

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -145,9 +145,9 @@ class HomeController < ApplicationController
 			result = result.composite(img) do |c|
 				  c.compose "Over"    # OverCompositeOp
 				  if img[:width] == 1008
-					  c.geometry "+1702+220" # copy second_image onto first_image from (20, 20)
+					  c.geometry "+1657+220" # copy second_image onto first_image from (20, 20)
 				  else
-					  c.geometry "+1576+220" # copy second_image onto first_image from (20, 20)
+					  c.geometry "+1531+220" # copy second_image onto first_image from (20, 20)
 				  end
 			end
 
