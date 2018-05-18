39344e6aff053b8872144099afce27045672ecc7
Linus Liang <linusliang@gmail.com>
2018-05-18 16:47:55 -0700

    change to 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,9 @@ class HomeController < ApplicationController
 	require 'mini_magick'
 	require 'tempfile'
 
-	$hashtag = 'ipasdf2'
-	$ipbucket  = 'instaprinter2'
-	$ipbackground = 'ip2_background.jpg'
+	$hashtag = 'ipasdf3'
+	$ipbucket  = 'instaprinter3'
+	$ipbackground = 'ip3_background.jpg'
                      
 	Aws.config.update({
 		region: 'us-west-1',
