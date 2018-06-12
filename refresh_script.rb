22403149e43d1e96bd5acf34c19c68a3460936f6
Linus Liang <linusliang@gmail.com>
2018-06-11 20:21:10 -0700

    walmart

--- refresh_script.rb
+++ refresh_script.rb
@@ -3,13 +3,13 @@
 while true
 	begin		
 		#production server 1
-		system('wget --quiet -O - http://production.4dyvwfpdk3.us-west-1.elasticbeanstalk.com/print_new_pics')
+		#system('wget --quiet -O - http://production.4dyvwfpdk3.us-west-1.elasticbeanstalk.com/print_new_pics')
 
 		#production server 2
 		#system('wget --quiet -O - http://production-2.us-west-1.elasticbeanstalk.com/print_new_pics')
 
 		#production server 3
-		#system('wget --quiet -O - http://production-4.us-west-1.elasticbeanstalk.com/print_new_pics')
+		system('wget --quiet -O - http://production-4.us-west-1.elasticbeanstalk.com/print_new_pics')
 		
 		sleep 15
 	rescue Exception => e 
