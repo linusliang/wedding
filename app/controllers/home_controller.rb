9d1e7994304376e49d4b96021cae61f4db6896d5
Linus Liang <linusliang@gmail.com>
2019-02-23 11:46:34 -0800

    lucy 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
 	require 'mini_magick'
 	require 'tempfile'
 
-	$hashtag = 'finallyagirl'
+	$hashtag = '#finallyagirlB'
 	$ipbucket  = 'instaprinter6'
 	$ipbackground = 'ip6_background.jpg'
                      
