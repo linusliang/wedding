772dc6d1f235dd915783f5444c5a012239758654
Linus Liang <linusliang@gmail.com>
2019-02-23 17:15:30 -0800

    back to lesby

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,9 @@ class HomeController < ApplicationController
 	require 'mini_magick'
 	require 'tempfile'
 
-	$hashtag = 'finallyagirlB'
-	$ipbucket  = 'instaprinter6'
-	$ipbackground = 'ip6_background.jpg'
+	$hashtag = 'MaysonTurns7'
+	$ipbucket  = 'instaprinter7'
+	$ipbackground = 'ip7_background.jpg'
                      
 	Aws.config.update({
 		region: 'us-west-1',
